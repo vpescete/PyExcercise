ba4f5e7f4627e30aa5fc5d27bffcd6d613c733fa
Valerio Pescetelli <valeriopescetellinl@gmail.com>
2023-02-15 14:45:43 +0100

    Fixed few issues

--- Py00/ex00.py
+++ Py00/ex00.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Scrivi una funzione che prenda in input due numeri e stampi ad output la somma tra due numeri.
 # Esempi di output:
 # -   14 + 16 = 30
@@ -10,16 +9,3 @@
 #Scrivi il tuo codice qui sotto ...
 
 def somma_nb(n1, n2):
-=======
-# Scrivi una funzione che prenda in input due numeri e stampi ad output la somma tra due numeri.
-# Esempi di output:
-# -   14 + 16 = 30
-# -   10 + 5 = 15
-# -   27 + 3 = 30
-#
-# Funzione ammesse: nessuna
-
-#Scrivi il tuo codice qui sotto ...
-
-def somma_nb(n1, n2):
->>>>>>> 9ce1c2ba069d44b4668c5a68c79e72bffcb9943b
--- Py00/ex07.py
+++ Py00/ex07.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # Creare una variabile ed assegnale un valore di tipo boolean. Stampa ad output il valore della variabile e il tipo della 
 # variabile stessa con un andare a capo tra il valore e il tipo.
 #
@@ -14,31 +13,3 @@
 # Scrivi il tuo codice qui sotto ...
 
 def print_bool(input_value):
-    if (input_value == True):
-        print("True")
-        print(type(input_value))
-    else:
-        print("False")
-=======
-# Creare una variabile ed assegnale un valore di tipo boolean. Stampa ad output il valore della variabile e il tipo della 
-# variabile stessa con un andare a capo tra il valore e il tipo.
-#
-# Esempi di output:
-# outuput 1 -->   True
-#                 <class 'bool'>
-# outuput 2 -->   False
-#                 <class 'bool'> 
-#
-# Funzione ammesse: print(), type()
-
-
-# Scrivi il tuo codice qui sotto ...
-
-def print_bool(input_value):
-    if (input_value == True):
-        print("True")
-        print(type(input_value))
-    else:
-        print("False")
->>>>>>> 9ce1c2ba069d44b4668c5a68c79e72bffcb9943b
-        print(type(input_value))
